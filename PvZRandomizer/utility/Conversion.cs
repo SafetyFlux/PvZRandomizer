5c573db55a0f1e2e3787c9e0b57aecc876e65759
SafetyFlux <dekuwither@gmail.com>
2022-12-15 17:03:15 -0700

    Add conversion class for converting int to hex string

---
+++ PvZRandomizer/utility/Conversion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvZRandomizer.utility
+{
+    static class Conversion
+    {
+        // Convert an integer into a hex string
+        public static string ConvertIntToHex(int num) => num.ToString("X4");
+    }
+}
