2bf5048cb5e9a0738814ecb669c606f1ba12b41a
SafetyFlux <dekuwither@gmail.com>
2023-01-20 09:49:37 -0700

    Add plant game data

---
+++ PvZRandomizer/Data/GameData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvZRandomizer.Data
+{
+    enum DataType { SunCost, RechargeRate, FiringRate, ProductionRate }
+
+    class GameData
+    {
+        // Constructor
+        public GameData(DataType type, int decStart, int defaultVal, int currentVal)
+        {
+            Type = type;
+            DecimalStart = decStart;
+            DefaultValue = defaultVal;
+            CurrentValue = currentVal;
+        }
+
+        // Properties
+        public DataType Type { get; set; }
+        public int DecimalStart { get; set; }
+        public int DefaultValue { get; set; }
+        public int CurrentValue { get; set; }
+    }
+}
---
+++ PvZRandomizer/Data/Plant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvZRandomizer.Data
+{
+    class Plant
+    {
+        // Constructor
+        public Plant(string name, string packetID, bool enabled)
+        {
+            Name = name;
+            SeedPacketID = packetID;
+            Enabled = enabled;
+            DataList = new List<GameData>();
+        }
+
+        // Properties
+        public string Name { get; set; }                // The plant's name
+        public string SeedPacketID { get; set; }        // The plant's seed packet ID (2-digit hex value)
+        public bool Enabled { get; set; }               // Whether randomizing the plant is enabled [NOT IMPLEMENTED]
+        public List<GameData> DataList { get; set; }    // A list of the plant's data values
+    }
+}
