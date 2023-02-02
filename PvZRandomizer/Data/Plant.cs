ead41532064f057174d5bb9072ae4648f20b2b41
SafetyFlux <dekuwither@gmail.com>
2023-02-02 09:10:28 -0700

    Rework Plant class

--- PvZRandomizer/Data/Plant.cs
+++ PvZRandomizer/Data/Plant.cs
@@ -8,19 +8,17 @@ namespace PvZRandomizer.Data
 {
     class Plant
     {
-        // Constructor
-        public Plant(string name, string packetID, bool enabled)
-        {
-            Name = name;
-            SeedPacketID = packetID;
-            Enabled = enabled;
-            DataList = new List<GameData>();
-        }
-
         // Properties
-        public string Name { get; set; }                // The plant's name
-        public string SeedPacketID { get; set; }        // The plant's seed packet ID (2-digit hex value)
         public bool Enabled { get; set; }               // Whether randomizing the plant is enabled [NOT IMPLEMENTED]
-        public List<GameData> DataList { get; set; }    // A list of the plant's data values
+        public string SeedPacketID { get; set; }        // The plant's seed packet ID (2-digit hex value)
+        public string EnglishName { get; set; }         // The plant's name
+        public SunCostData SunCost { get; set; }
+        public RechargeRateData RechargeRate { get; set; }
+
+        // Overridden ToString method
+        public override string ToString()
+        {
+            return $"{SeedPacketID} - {EnglishName}";
+        }
     }
 }
---
+++ PvZRandomizer/Data/RechargeRateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvZRandomizer.Data
+{
+    class RechargeRateData
+    {
+        public int DecimalStart { get; set; }
+        public int DefaultValue { get; set; }
+        public int CurrentValue { get; set; }
+    }
+}
